066255db3335cf68ae547709aa021109b187c020
MichaelBro <michael.bro@mail.ru>
2021-03-08 11:30:36 +0300

    Added trace

--- cmd/main.go
+++ cmd/main.go
@@ -4,9 +4,27 @@ import (
 	"bgo-homeworks-07/pkg/bank"
 	"bgo-homeworks-07/pkg/processing"
 	"fmt"
+	"log"
+	"os"
+	"runtime/trace"
 )
 
 func main() {
+	f, err := os.Create("trace.out")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() {
+		if err := f.Close(); err != nil {
+			log.Print(err)
+		}
+	}()
+	err = trace.Start(f)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer trace.Stop()
+
 	transactions := bank.GenerateRandomTransactions(100000)
 	userId := uint64(5)
 	g := 5
